790cf197d97e49cb8b60c99ab538e369c788b852
Yoan <yoandiny14@gmail.com>
2025-02-24 08:18:31 +0300

    index change

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+const hireButton = document.getElementById("hireButton");
+const projectButton = document.getElementById("projectButton");
+  
+projectButton.addEventListener("click", () => {
+  window.location.href = "achieve.html";
+});
+  
+  
+  
+  
+  
+  
---
+++ assets/js/nav.js
@@ -0,0 +1,13 @@
+document.getElementById("nav").innerHTML = `
+<nav>
+ <ul>
+    <li><a href="index.html">Home</a></li>
+    <li class="AboutMe"><a href="#AboutMe">About Me</a></li>
+    <li class="Education"><a href="#education">Education</a>
+    </li>
+    <li><a href="achieve.html">Projects</a></li>
+    <li><a href="#contact">Conctact</a></li>
+   
+</ul>
+</nav>
+`;
