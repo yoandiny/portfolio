147f7410d155e8f3b37e29050adf0075bdae63cf
Yoan <yoandiny14@gmail.com>
2025-02-25 00:11:17 +0300

    Complete change of art direction

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -3,8 +3,6 @@ document.getElementById("nav").innerHTML = `
  <ul>
     <li><a href="index.html">Home</a></li>
     <li class="AboutMe"><a href="#AboutMe">About Me</a></li>
-    <li class="Education"><a href="#education">Education</a>
-    </li>
     <li><a href="achieve.html">Projects</a></li>
     <li><a href="#contact">Conctact</a></li>
    
